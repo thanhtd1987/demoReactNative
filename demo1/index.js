a063da02fc838cf061d5c03631eb6f75a207c54e
thanhtd1987 <anhbin238@yahoo.com>
2017-10-29 23:59:39 +0700

    [GENERAL] change minor to build correctly

--- demo1/index.js
+++ demo1/index.js
@@ -1,6 +1,6 @@
 // import { AppRegistry } from 'react-native';
 // import App from './App';
-import Calculator from './ex_calculator';
+// import Calculator from './ex_calculator';
 
 import React, {Component} from "react";
 import {
@@ -25,11 +25,42 @@ class Demo1 extends Component{
         return(
             // <StatusBar hidden="true"></StatusBar>
             
-            <View style={myStyle.bao}>
-                <Text style={myStyle.tomtat}>Hello world!</Text>
-                <Text style={myStyle.tieude}>Text 2, 2 style</Text>
-                <Text style={{backgroundColor:'violet',flex:1}}></Text>
+            // <View style={myStyle.bao}>
+            //     <Text style={myStyle.tomtat}>Hello world!</Text>
+            //     <Text style={myStyle.tieude}>Text 2, 2 style</Text>
+            //     <Text style={{backgroundColor:'violet',flex:1}}></Text>
+            // </View>
+
+        <View style={{flexDirection:"column",flex:1}}>
+            <View style={myStyle.item_column}>
+                <View style={myStyle.item_row}>
+                    <Text style={myStyle.big_number}>2</Text>
+                    <Text style={myStyle.small_number}>ABC</Text>
+                </View>
+                <View style={myStyle.item_row}>
+                    <Text style={myStyle.big_number}>2</Text>
+                    <Text style={myStyle.small_number}>ABC</Text>
+                </View>
+                <View style={myStyle.item_row}>
+                    <Text style={myStyle.big_number}>2</Text>
+                    <Text style={myStyle.small_number}>ABC</Text>
+                </View>
             </View>
+            <View style={myStyle.item_column}>
+                <View style={myStyle.item_row}></View>
+                <View style={myStyle.item_row}></View>
+                <View style={myStyle.item_row}></View></View>
+            <View style={myStyle.item_column}>
+                <View style={myStyle.item_row}></View>
+                <View style={myStyle.item_row}></View>
+                <View style={myStyle.item_row}></View></View>
+            <View style={myStyle.item_column}>
+                <View style={myStyle.item_row}></View>
+                <View style={myStyle.item_row}></View>
+                <View style={myStyle.item_row}></View>
+            </View>
+            
+        </View>
         );
     }
 
@@ -59,7 +90,38 @@ var myStyle = StyleSheet.create({
         color:'blue',
         // marginTop:30,
         flex:3
+    },
+
+    item_column:{        
+        borderBottomColor:'red',
+        borderBottomWidth:1,
+        flex:1,
+        flexDirection:'row'
+    },
+    item_row:{
+        flex:1,
+        // marginTop:10,
+        // marginBottom:10,
+        // justifyContent:'center',
+        // alignContent:'center',
+        borderBottomColor:'red',
+        borderBottomWidth:1,
+        backgroundColor:'gray',
+        
+    },
+    big_number:{
+        // justifyContent:'center',
+        // alignContent:'center',
+        fontSize:40,
+        flex:1
+    },
+    small_number:{
+        // justifyContent:'center',
+        // alignContent:'center',
+        flex:1, 
+        color:'blue'
     }
+
 });
 
-AppRegistry.registerComponent('demo1', () => Calculator);
+AppRegistry.registerComponent('demo1', () => Demo1);
