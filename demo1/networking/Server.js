6e76b01393ef92849b6d56136733c4a2b4c32778
ThanhTD <thanht.tranduy@neo-lab.vn>
2018-04-16 07:14:13 +0700

    add POST function

--- demo1/networking/Server.js
+++ demo1/networking/Server.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import { AppRegistry, SectionList, StyleSheet, Text, View, Alert, Platform } from 'react-native';
-const apiGetAllTeas = 'https://my-json-server.typicode.com/thanhtd1987/demoReactNative/db'
+
+const apiGetAllTeas = 'https://my-json-server.typicode.com/thanhtd1987/demoReactNative/teas'
+const apiInsertTea = 'https://my-json-server.typicode.com/thanhtd1987/demoReactNative/teas'
+
 async function getTeaFromServer(params) {
     try {
         let response = await fetch(apiGetAllTeas)
         let responseJson = await response.json()
-        return responseJson.teas //list of teas
+        return responseJson //list of teas
+    } catch (error) {
+        console.error(`Error: ${error}`)
+    }
+}
+
+async function insertNewTeaToServer (param) {
+    try {
+        let response = await fetch(apiInsertTea, {
+            merthod: 'POST',
+            headers: {
+                'Accept':'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                name:'king of tea',
+                description: 'tea with many flavors',
+                image: '',
+                id: param
+            })
+        })
     } catch (error) {
         console.error(`Error: ${error}`)
     }
